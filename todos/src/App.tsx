b1c8cf9011f9d476cf1587f40ecdf72e4397859f
litophoto <ciro17581@gmail.com>
2022-09-06 01:36:16 +0900

    Fine-tuning of minor settings.

--- todos/src/App.tsx
+++ todos/src/App.tsx
@@ -1,26 +1,11 @@
-import React, { useEffect, useState } from "react";
-import { Todo } from "./models/Todo";
-import { useFetch } from "./hooks/useFetch";
-import { getTodos, postTodo } from "./hooks/todosHooks";
 import OnePageTodos from "./OnePageTodos";
+import { TodoList } from "./TodosApp/components/TodoList";
 
 function App() {
-  // const [todoList, setTodoList] = useState<Todo[]>([
-  //   { id: 1, title: "aaa", done: true },
-  // ]);
-  // const { data: todos } = useFetch<Todo[]>(
-  //   "http://127.0.0.1:8000/api/v1/todos/"
-  // );
-  // useEffect(() => {
-  //   console.log(todos);
-  //   console.log(todoList);
-  // }, []);
-  // useEffect(() => {
-  //   setTodoList(todos)
-  // }, [])
   return (
     <>
-      <OnePageTodos></OnePageTodos>
+      <OnePageTodos />
+      {/* <TodoList></TodoList> */}
     </>
   );
 }
