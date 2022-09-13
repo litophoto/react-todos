53633070b8ba86c0e45e1b78302ddfe240fd5e7b
litophoto <ciro17581@gmail.com>
2022-09-13 12:56:00 +0900

    [create] model, api and 'ListItem', 'Modal' components

---
+++ todos-9-12/src/TodosApp/components/TodoListItem.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { Todo } from "../models/Todo";
+
+const TodoListItem = (todo: Todo) => {
+  return <></>
+}
+
+export default TodoListItem
---
+++ todos-9-12/src/TodosApp/services/TodosAPI.tsx
@@ -0,0 +1,61 @@
+import { useState, useEffect } from "react";
+import { Todo } from "../models/Todo";
+
+const url = "http://127.0.0.1:8000/api/v1/todos/";
+const headers = { "Content-Type": "application/json" };
+export const useTodosAPI = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(url);
+      const data = await response.json();
+      setTodos(data);
+    })();
+  }, []);
+  const postTodo = (todo: Todo) => {
+    (async () => {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify(todo),
+      });
+      const data = await response.json();
+      setTodos([data, ...todos]);
+    })();
+  };
+  const putTodo = (todoId: number, newTodo: Todo) => {
+    (async () => {
+      const response = await fetch(`${url}${todoId}/`, {
+        method: "PUT",
+        headers: headers,
+        body: JSON.stringify(newTodo),
+      });
+      const data = await response.json();
+      const newTodos = todos.map((todo) => {
+        if (todo.id === data.id) return data;
+        return todo;
+      });
+      setTodos(newTodos);
+    })();
+  };
+  const deleteTodo = (todoId: number) => {
+    (async () => {
+      const response = await fetch(`${url}${todoId}/`, {
+        method: "DELETE",
+        headers: headers,
+      });
+      const data = await response.json();
+      const newTodos = todos.filter((todo) => {
+        if (todo.id === data.id) return false;
+        return true;
+      });
+      setTodos(newTodos);
+    })();
+  };
+  const actions = {
+    add: postTodo,
+    edit: putTodo,
+    delete: deleteTodo,
+  };
+  return [todos, actions] as const;
+};
